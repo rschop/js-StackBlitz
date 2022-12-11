97ad2df90d0034ca7d2713533a90b9cb98b74fe5
rschop <rschop@users.noreply.github.com>
2022-12-11 09:23:07 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,4 +5,21 @@
 // const appDiv = document.getElementById('app');
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-console.log('hello world')
+const timestamp = new Date().getTime();
+console.log(timestamp); // 👉️ 1642664853302
+
+const date = new Date(timestamp);
+console.log(date); // 👉️ Thu Jan 20 2022 09:48:00
+
+console.log(date.toString()); // 👉️ "Thu Jan 20 2022 09:48:00"
+
+// 👇️ Format date and time using different locales
+console.log(date.toLocaleString('en-US')); // 👉️ "1/20/2022, 9:50:15 AM"
+console.log(date.toLocaleString('en-GB')); // 👉️ "20/01/2022 09:50:15"
+console.log(date.toLocaleString('sv')); // 👉️ "2022-01-20 09:50:15"
+
+// 👇️ Display only date
+console.log(date.toLocaleDateString('en-US')); // 👉️ "1/20/2022"
+
+// 👇️ Display only time
+console.log(date.toLocaleTimeString('en-US')); // 👉️ "9:50:15 AM"
